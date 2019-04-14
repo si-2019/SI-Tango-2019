b2bc603333e2a0b4b2ed3b55b541bffc6a5f5105
Samra <ssalihic1@etf.unsa.ba>
2019-04-14 19:11:36 +0200

    Konekcija na bazu

---
+++ models/db.js
@@ -0,0 +1,15 @@
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP",{host:'37.59.55.185',dialect:"mysql",logging:false,  port: 3306,
+});
+const db = {}
+db.Sequelize = Sequelize;  
+db.sequelize = sequelize;
+
+//import modela
+db.comment = sequelize.import(__dirname+'/Comment.js');
+db.theme = sequelize.import(__dirname+'/Theme.js');
+db.reply = sequelize.import(__dirname+'/Reply.js');
+db.sticky = sequelize.import(__dirname+'/Sticky.js');
+
+module.exports=db;
