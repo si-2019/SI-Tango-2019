6ce51bdbf8605b8b2728c3f2418c9749f7679f03
amasovic1 <amasovic1@etf.unsa.ba>
2019-04-15 17:10:05 +0200

    Dodavanje teme

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//dodavanje teme
+app.post('/addTheme', function(req, res) {
+	  console.log("addThemePOST IdPredmeta" + req.body.IdPredmeta);
+	  console.log("addThemePOST IdUser "+ req.body.IdUser);
+	  console.log("addThemePOST Title "+ req.body.title);
+	  console.log("addThemePOST Description"+ req.body.description);
+	  console.log("addThemePOST Closed"+ req.body.closed);
+	  console.log("addThemePOST TimeCreated"+ req.body.timeCreated);
+	  db.theme.findOrCreate({where: {title: req.body.Title} , defaults: {description: req.body.description, closed: false,
+	   timeCreated: req.body.timeCreated}}).then(([user, created]) => {
+		    if (created) {
+		    	console.log("Uspjesno kreirana tema");
+		    }
+		    else{
+		    	console.log("Tema sa datim nazivom vec postoji");	
+		    }    	
+		});
+};
 
 app.listen(PORT);
 
