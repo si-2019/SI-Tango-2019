84715d748e6452e61f65bb2035e922be312294ec
Samra Salihic <37187220+ssalihic@users.noreply.github.com>
2019-04-17 00:13:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,35 @@ db.sequelize.sync().then(function(){
 
 const app = express();
 
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(PORT);
+app.post('/addTheme', function(req, res) {
+	  console.log("addThemePOST IdPredmeta" + req.body.IdPredmeta);
+	  console.log("addThemePOST IdUser "+ req.body.IdUser);
+	  console.log("addThemePOST Title "+ req.body.title);
+	  console.log("addThemePOST Description"+ req.body.description);
 
+	  console.log("addThemePOST TimeCreated"+ req.body.timeCreated);
+	  db.theme.findOrCreate({where: {title: req.body.title} , defaults: {description: req.body.description, closed: false,
+	   timeCreated:Date.now(), idPredmet:req.body.IdPredmeta,idUser:req.body.IdUser}}).then(([user, created]) => {
+		    if (created) {
+		    	console.log("Uspjesno kreirana tema");
+		    }
+		    else{
+		    	console.log("Tema sa datim nazivom vec postoji");	
+		    }    	
+		});
+});
+
+app.listen(PORT);
